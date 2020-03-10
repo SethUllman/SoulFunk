ba2e321285b11d3e17513c66bf136431f5cf3362
Seth Ullman <seth.ullman98@gmail.com>
2020-03-10 11:49:04 -0600

    add eventListener to soulfunk.jsx

---
+++ frontend/soulfunk.jsx
@@ -0,0 +1,18 @@
+import React from '../node_modules/react';
+import ReactDOM from '../node_modules/react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
+
+document.addEventListener("DOMContentLoaded", () => {
+  let preloadedState = undefined;
+  if (window.currentUser) {
+    preloadedState = {
+      session: {
+        currentUser: window.currentUser
+      }
+    }
+  }
+  const root = document.getElementById('root');
+  const store = configureStore(preloadedState);
+  ReactDOM.render(<Root store={store}/>, root);
+})
