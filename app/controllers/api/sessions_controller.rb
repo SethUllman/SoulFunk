c22dae32c7d94f7ee7b9a43dd562c11543900110
Seth Ullman <seth.ullman98@gmail.com>
2020-04-02 16:17:30 -0600

    remove debuggers

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,12 +2,9 @@ class Api::SessionsController < ApplicationController
 
   def create
     @administrator = Administrator.find_by_credentials(params[:administrator][:username], params[:administrator][:password])
-    byebug
     if @administrator.nil?
-      debugger
       render json: ['Invalid username or password'], status: 401
     else
-      debugger
       login(@administrator)
       render 'api/administrators/show'
     end
