8b57ad3b2be3ded1f94bbafa0fa2e6d85941b107
Seth Ullman <seth.ullman98@gmail.com>
2020-04-21 23:33:13 -0600

    update member_actions.js

---
+++ frontend/actions/member_actions.js
@@ -0,0 +1,6 @@
+import {postMember, removeMember, updateMember} from '../util/member_api_util';
+
+export const RECEIVE_MEMBER = 'RECEIVE_MEMBER';
+export const REMOVE_MEMBER = 'REMOVE_MEMBER';
+export const UPDATE_MEMBER = 'UPDATE_MEMBER';
+
