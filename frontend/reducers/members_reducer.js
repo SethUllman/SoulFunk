6ed8898e18bcc79397665f46eb96ed4c6bcda0e6
Seth Ullman <seth.ullman98@gmail.com>
2020-04-22 21:00:42 -0600

    add members_reducer

---
+++ frontend/reducers/members_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_MEMBERS, CREATE_MEMBER, REMOVE_MEMBER, UPDATE_MEMBER } from '../actions/member_actions';
+
+export default (state = null, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_MEMBERS:
+      return Object.assign({}, { members: action.members});
+    case CREATE_MEMBER:
+      return state;
+    case REMOVE_MEMBER:
+      return state;
+    case UPDATE_MEMBER:
+      return state;
+    default:
+      return state;
+  }
+};
