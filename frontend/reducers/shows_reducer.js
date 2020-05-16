7ddad99b7b8708a662d51d55723abcbcef94f17c
Seth Ullman <seth.ullman98@gmail.com>
2020-05-15 23:55:48 -0600

    add shows reducer

---
+++ frontend/reducers/shows_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_SHOWS, CREATE_SHOW, REMOVE_SHOW, UPDATE_SHOW } from '../actions/show_actions';
+
+export default (state = null, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_SHOWS:
+      return action.shows;
+    case CREATE_SHOW:
+      return Object.assign([], state, {[action.show.id]: action.show});
+    case REMOVE_SHOW:
+      return state;
+    case UPDATE_SHOW:
+      return state;
+    default:
+      return state;
+  }
+};
