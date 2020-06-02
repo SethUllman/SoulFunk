934062b5d2894a0bd11dbbc40153248737ba455d
Seth Ullman <seth.ullman98@gmail.com>
2020-06-01 23:47:35 -0600

    add charge to form

--- frontend/components/shows/show_form.js
+++ frontend/components/shows/show_form.js
@@ -1,12 +1,17 @@
 import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import {createShow} from '../../actions/show_actions';
 
 const showForm = () => {
 
   const [location, setLocation] = useState('');
   const [time, setTime] = useState('');
+  const [charge, setCharge] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const show = {}
   }
 
   return (
@@ -14,13 +19,27 @@ const showForm = () => {
       <form onSubmit={handleSubmit}>
         <label>
           Location: 
-          <input type='text'>
-
+          <input 
+            type='text'
+            value={location}
+            onChange={() => {setLocation(e.target.value)}}>
           </input>
         </label>
         <label>
           Time:
-          <input type='date-time'>
+          <input 
+            type='date-time'
+            value={time}
+            onChange={() => {setTime(e.target.value)}}>
+
+          </input>
+        </label>
+        <label>
+          Charge:
+          <input 
+            type='text'
+            value={charge}
+            onChange={() => {setCharge(e.target.value)}}>
 
           </input>
         </label>
