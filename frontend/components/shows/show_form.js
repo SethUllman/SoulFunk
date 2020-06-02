edfabf3159982b4d35b65803c043d4865732af2f
Seth Ullman <seth.ullman98@gmail.com>
2020-06-01 23:37:53 -0600

    add form element

--- frontend/components/shows/show_form.js
+++ frontend/components/shows/show_form.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 const showForm = () => {
 
-  return null;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Location: 
+          <input type='text'>
+
+          </input>
+        </label>
+        <label>
+          Time:
+          <input type='date-time'>
+            
+          </input>
+        </label>
+      </form>
+    </div>
+  );
 }
 
 export default showForm;
