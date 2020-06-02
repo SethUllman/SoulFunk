052e384b1d1bcb34fd1b8054570b0217912c3084
Seth Ullman <seth.ullman98@gmail.com>
2020-06-01 23:51:45 -0600

    add showIndex component

---
+++ frontend/components/shows/index.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import {reaceiveShows} from '../../actions/show_actions';
+
+const showIndex = () => {
+  return null;
+}
+
+export default showIndex;
