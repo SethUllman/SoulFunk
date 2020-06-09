85b26bdde6da2127df659ff262b40903c29e40e4
Seth Ullman <seth.ullman98@gmail.com>
2020-06-09 01:14:47 -0600

    update nav for mobile first development

--- frontend/components/navigation/navigation.jsx
+++ frontend/components/navigation/navigation.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
 import { logout } from '../../actions/session_actions';
+import { FaBars } from 'react-icons/fa';
 
 const Nav = () => {
 
@@ -23,14 +24,18 @@ const Nav = () => {
   return(
     <div className='nav-div'>
       <div>{welcomeMessage()}</div>
+      <div className='nav-drop'>
+        <FaBars className='nav-bars' size = '112px'></FaBars>
+        <ul className='nav-ul'>
+          <li onClick={() => {history.push("/home")}}>Home</li>
+          <li onClick={() => {history.push("/shows")}}>Shows</li>
+          <li onClick={() => {history.push("/gallery")}}>Gallery</li>
+          <li onClick={() => {history.push("/members")}}>The Band</li>
+          <li onClick={() => {history.push("/contact")}}>Contact</li>
+        </ul>
+      </div>
       <div className='nav-logo'></div>
-      <ul className='nav-ul'>
-        <li onClick={() => {history.push("/home")}}>Home</li>
-        <li onClick={() => {history.push("/shows")}}>Shows</li>
-        <li onClick={() => {history.push("/gallery")}}>Gallery</li>
-        <li onClick={() => {history.push("/members")}}>The Band</li>
-        <li onClick={() => {history.push("/contact")}}>Contact</li>
-      </ul>
+      
     </div>
   );
 
