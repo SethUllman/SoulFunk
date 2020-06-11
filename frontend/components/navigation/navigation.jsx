d748b0d5069c5bfca410dc4bc7a9cfd534fc2b72
Seth Ullman <seth.ullman98@gmail.com>
2020-06-11 01:10:39 -0600

    add close button to nav

--- frontend/components/navigation/navigation.jsx
+++ frontend/components/navigation/navigation.jsx
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
 import { logout } from '../../actions/session_actions';
 import { FaBars } from 'react-icons/fa';
+import { MdClose } from 'react-icons/md';
 
 const Nav = () => {
 
@@ -23,7 +24,7 @@ const Nav = () => {
 
   window.onclick = function(event) {
     if (!event.target.matches('.nav-drop')) {
-      let dropdowns = document.getElementsByClassName("nav-ul");
+      let dropdowns = document.getElementsByClassName("nav-modal");
       let i;
       for (i = 0; i < dropdowns.length; i++) {
         let openDropdown = dropdowns[i];
@@ -35,11 +36,18 @@ const Nav = () => {
   }
 
   const dropDown = () => {
-    let nav = document.getElementById('nav-ul');
+    let nav = document.getElementById('nav-modal');
     if (nav.className.indexOf('show') == -1) {
       nav.className += 'show';
     } else {
       nav.className = nav.className.replace('show', '');
+    }
+  }
+
+  const closeModal = () => {
+    let nav = document.getElementById('nav-modal');
+    if (nav.className == 'nav-modalshow') {
+      nav.className = 'nav-modal';
     }
   }
 
@@ -48,17 +56,33 @@ const Nav = () => {
       <div>{welcomeMessage()}</div>
       <div className='nav-drop'>
         <FaBars className='nav-bars' size='45px' onClick={dropDown}></FaBars>
-        <ul id='nav-ul' className='nav-ul'>
-          <li className='nav-li' onClick={() => {history.push("/home")}}>Home</li>
-          <li className='nav-li' onClick={() => {history.push("/shows")}}>Shows</li>
-          <li className='nav-li' onClick={() => {history.push("/gallery")}}>Gallery</li>
-          <li className='nav-li' onClick={() => {history.push("/members")}}>The Band</li>
-          <li className='nav-li' onClick={() => {history.push("/contact")}}>Contact</li>
+        <ul id='nav-modal' className='nav-modal'>
+          <MdClose size='35px' className='nav-close' onClick={closeModal}></MdClose>
+          <li className='nav-li' onClick={() => {
+            closeModal();
+            history.push("/home")}}>Home
+          </li>
+          <li className='nav-li' onClick={() => {
+            closeModal();
+            history.push("/shows")}}>Shows
+          </li>
+          <li className='nav-li' onClick={() => {
+            closeModal();
+            history.push("/gallery")}}>Gallery
+          </li>
+          <li className='nav-li' onClick={() => {
+            closeModal();
+            history.push("/members")}}>The Band
+          </li>
+          <li className='nav-li' onClick={() => {
+            closeModal();
+            history.push("/contact")}}>Contact
+          </li>
         </ul>
       </div>
       <div className='nav-logo'></div>
-      
     </div>
+    
   );
 
 }
