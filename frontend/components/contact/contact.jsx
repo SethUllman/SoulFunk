f235f77166bc03288c406bd759325dba846563f0
Seth Ullman <seth.ullman98@gmail.com>
2020-07-20 14:36:31 -0600

    add email field to contact

--- frontend/components/contact/contact.jsx
+++ frontend/components/contact/contact.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import useHistory from 'react-router-dom';
 
 const contact = () => {
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [email, setEmail] = useState('');
+
+
 
   return(
     <div className='form-div'>
       <form className='form'>
         <h2>Contact</h2>
         <label>
+          Email or Phone Number:
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}  
+          ></input>
+        </label>
+        <label>
           Title:
-          <input type='text'></input>
+          <input 
+            type='text'
+            value={title}
+            onChange={(e) => setTitle(e.currentTarget.value)}
+            ></input>
         </label>
         <label id='text-label'>
           Body:
-          <textarea/>
+          <textarea 
+            value={body}
+            onChange={(e) => setBody(e.currentTarget.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
