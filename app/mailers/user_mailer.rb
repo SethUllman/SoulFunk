d80ce829f0f36f29926fd395c097ba884b9f6afe
Seth Ullman <seth.ullman98@gmail.com>
2020-07-20 17:28:59 -0600

    remove unnecessary mailer code

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  def welcome_email
-
-    @message = @message
-
-    mail(to: 'seth.ullman98@gmail.com', subject: 'Message from a Fan')
-  end
-end
--- frontend/components/contact/contact.jsx
+++ frontend/components/contact/contact.jsx
@@ -5,50 +5,41 @@ import emailjs from 'emailjs-com';
 
 const contact = () => {
 
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const [email, setEmail] = useState('');
+
   const history = useHistory();
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     
-    emailjs.sendForm('gmail', 'template_ERJ64dUc', e.target, 'user_Zc5AFeBhwbtAjm5JKVfCS')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
+    emailjs.sendForm('gmail', 'template_ERJ64dUc', '#contact-form', 'user_Zc5AFeBhwbtAjm5JKVfCS')
+      .then(() => {
+        history.push('/home');
       });
-
-
   }
 
 
   return(
     <div className='form-div'>
-      <form className='form' onSubmit={handleSubmit}>
+      <form id='contact-form' className='form' onSubmit={handleSubmit}>
         <h2>Contact</h2>
         <label>
           Email or Phone Number:
           <input
-            value={email}
-            onChange={(e) => setEmail(e.currentTarget.value)}  
+            name='email'
           ></input>
         </label>
         <label>
           Title:
           <input 
+            name='title'
             type='text'
-            value={title}
-            onChange={(e) => setTitle(e.currentTarget.value)}
             ></input>
         </label>
         <label id='text-label'>
           Body:
           <textarea 
-            value={body}
-            onChange={(e) => setBody(e.currentTarget.value)}
+            name='body'
           />
         </label>
         <button type='submit'>Submit</button>
